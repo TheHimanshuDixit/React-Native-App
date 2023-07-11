75f236debaf8bc72e7914bb6d424fbfb889a0036
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-07-11 23:50:30 +0530

    some jsx concepts

--- src/screens/components/start.js
+++ src/screens/components/start.js
@@ -1,16 +1,28 @@
 import { StyleSheet, Text } from 'react-native';
 
 const Start = () => {
+
+  const a = "Himanshu";
+  const b = <Text>Start</Text>;
+
+  const getfullname = (fname, lname) => {
+    return fname + " " + lname;
+  }
+
   return (
-    <Text style={styles.textstyle}>Start</Text>
+    <>
+      <Text style={styles.textstyle}>Start {a}</Text>
+      {b}
+      <Text style={styles.textstyle}>{getfullname("Himanshu", "Dixit")}</Text>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-    textstyle: {
-        fontSize: 30,
-        color: 'yellow'
-    }
+  textstyle: {
+    fontSize: 30,
+    color: 'yellow'
+  }
 })
 
 export default Start;
