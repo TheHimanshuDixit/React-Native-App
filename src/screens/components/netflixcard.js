d5210ced460823358642b91cfa73ac3e697e0306
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-07-13 19:48:55 +0530

    netfilxcard & font & SplashScreen react native

--- src/screens/components/netflixcard.js
+++ src/screens/components/netflixcard.js
@@ -1,28 +1,72 @@
 import { Button, Image, Linking, StyleSheet, Text, View } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen';
+import {
+    useFonts,
+    JosefinSans_100Thin,
+    JosefinSans_200ExtraLight,
+    JosefinSans_300Light,
+    JosefinSans_400Regular,
+    JosefinSans_500Medium,
+    JosefinSans_600SemiBold,
+    JosefinSans_700Bold,
+    JosefinSans_100Thin_Italic,
+    JosefinSans_200ExtraLight_Italic,
+    JosefinSans_300Light_Italic,
+    JosefinSans_400Regular_Italic,
+    JosefinSans_500Medium_Italic,
+    JosefinSans_600SemiBold_Italic,
+    JosefinSans_700Bold_Italic,
+} from '@expo-google-fonts/josefin-sans';
+
+SplashScreen.preventAutoHideAsync();
 
 const Netflixcard = () => {
-    return (
-        <View>
-            <Text style={styles.header}>
-                Netflix Card
-            </Text>
-            <View style={styles.poster}>
-                <Image
-                    style={styles.tinyLogo}
-                    source={{
-                        uri: "https://th.bing.com/th/id/OIP.aC6Rd5Y-3zoj2ECGPs19ZQHaKY?w=113&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-                    }}
-                />
-                <Text style={styles.name}>War</Text>
-                <Text style={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil numquam doloremque facere aut fuga, eaque dolore non, quos ex magnam reiciendis maiores laboriosam dolor id quas beatae, exercitationem neque quaerat.</Text>
-                <Button
-                    color={'#E50914'}
-                    title='Watch Now'
-                    onPress={() => { Linking.openURL('https://www.netflix.com/in/title/80223226') }}
-                />
+    let [fontsLoaded] = useFonts({
+        JosefinSans_100Thin,
+        JosefinSans_200ExtraLight,
+        JosefinSans_300Light,
+        JosefinSans_400Regular,
+        JosefinSans_500Medium,
+        JosefinSans_600SemiBold,
+        JosefinSans_700Bold,
+        JosefinSans_100Thin_Italic,
+        JosefinSans_200ExtraLight_Italic,
+        JosefinSans_300Light_Italic,
+        JosefinSans_400Regular_Italic,
+        JosefinSans_500Medium_Italic,
+        JosefinSans_600SemiBold_Italic,
+        JosefinSans_700Bold_Italic,
+    });
+
+    if (!fontsLoaded) {
+        return <Text>Loading...</Text>;
+    }
+
+    else {
+        SplashScreen.hideAsync();
+        return (
+            <View>
+                <Text style={styles.header}>
+                    Netflix Card
+                </Text>
+                <View style={styles.poster}>
+                    <Image
+                        style={styles.tinyLogo}
+                        source={{
+                            uri: "https://th.bing.com/th/id/OIP.aC6Rd5Y-3zoj2ECGPs19ZQHaKY?w=113&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+                        }}
+                    />
+                    <Text style={styles.name}>War</Text>
+                    <Text style={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil numquam doloremque facere aut fuga, eaque dolore non, quos ex magnam reiciendis maiores laboriosam dolor id quas beatae, exercitationem neque quaerat.</Text>
+                    <Button
+                        color={'#E50914'}
+                        title='Watch Now'
+                        onPress={() => { Linking.openURL('https://www.netflix.com/in/title/80223226') }}
+                    />
+                </View>
             </View>
-        </View>
-    )
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -31,6 +75,7 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: 'bold',
         padding: 10,
+        fontFamily: 'JosefinSans_100Thin',
     },
     tinyLogo: {
         width: "100%",
@@ -52,6 +97,7 @@ const styles = StyleSheet.create({
     text: {
         marginBottom: 20,
         paddingHorizontal: 20,
+        fontFamily: 'JosefinSans_100Thin',
     }
 })
 
